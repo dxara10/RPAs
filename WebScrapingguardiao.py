300f6eab4a15934bf0e027682d260994f02f0011
Douglas Cortes <135165192+dxara10@users.noreply.github.com>
2024-10-08 16:31:18 -0300

    Update WebScrapingguardiao.py

--- WebScrapingguardiao.py
+++ WebScrapingguardiao.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 import pandas as pd
 import sqlite3
+import datetime as dt
+import time as tm
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
 
+# Conexão com o banco de dados SQLite
 conexao = sqlite3.connect('Banco_CHAMADAS.db')
 c = conexao.cursor()
-c.execute(''' CREATE TABLE CHAMADAS (
+
+# Criação da tabela CHAMADAS
+c.execute('''
+CREATE TABLE IF NOT EXISTS CHAMADAS (
     ALVO text,
     TERMINAL int,
     TELINTERLOCUTOR int,
@@ -15,138 +27,79 @@ c.execute(''' CREATE TABLE CHAMADAS (
     DATA date,
     HORAINICIAL time,
     HORAFINAL time,
-    DURAÇÃO time)
-    ''')
+    DURACAO time
+)
+''')
 conexao.commit()
 conexao.close()
 
+# Definição do diretório de chamadas
+diretorio = r"C:\Users\PC\Desktop\CLIENTE\CHAMADAS"
 
-#PROCESSODE RASPAGEM EM SI
-
-import datetime as dt
-import time as tm
-import os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import sqlite3
-
-#ATENÇÃO: no próprio computador do Antônio eu fiz algumas alterações no código para este interagir com o usuário, exemplo aqui no diretório, eu chamei a função input(“perguntando a pasta a ser compilada”), atribuindo a mesma à variável diretorio
-diretorio = r"C:\Users\PC\Desktop\CLIENTE\CHAMADAS"        
-
-
+# Processo de raspagem dos arquivos HTML no diretório
 for arquivo in os.listdir(diretorio):
     if arquivo.endswith(".html"):
         caminho = os.path.join(diretorio, arquivo)
-        from selenium.webdriver.chrome.options import Options
+
+        # Configuração do navegador em modo headless
         chrome_options = Options()
-        chrome_options.headless = True 
+        chrome_options.headless = True
         navegador = webdriver.Chrome(options=chrome_options)
         navegador.get("file:///" + caminho)
-        
-        ALVO = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/caption').get_attribute("textContent")
 
-        TERMINAL = navegador.find_element(By.XPATH,
-            '/html/body/center/table[1]/tbody/tr[2]/td[1]').get_attribute("textContent")
-        t1 = TERMINAL[3:5] + TERMINAL[6:14]
-        TERMINAL = t1
+        # Extração de dados do arquivo HTML
+        ALVO = navegador.find_element(By.XPATH, '/html/body/center/table[2]/caption').get_attribute("textContent")
 
-        TELINTERLOCUTOR = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[4]').get_attribute("textContent")
+        TERMINAL = navegador.find_element(By.XPATH, '/html/body/center/table[1]/tbody/tr[2]/td[1]').get_attribute("textContent")
+        TERMINAL = TERMINAL[3:5] + TERMINAL[6:14]
 
-        interlocutores = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[4]').get_attribute("textContent")
+        TELINTERLOCUTOR = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[4]').get_attribute("textContent")
+
+        interlocutores = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[4]').get_attribute("textContent")
         INTERLOCUTORES = interlocutores + "/" + TERMINAL
 
-        #adaptar
-        AUDIO = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[9]').get_attribute("textContent")
-        a2 = "C:\\Users\\PC\\Desktop\\CLIENTE\\Gravacoes\\Gravacoes\\"
-        a3 = '.wav'
-        a1 = a2 + AUDIO[0:8] + a3
-        AUDIO = a1
-        
-        
-        MENSAGEM = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[2]/td/table/tbody/tr[2]/td').get_attribute("textContent")
-        
-            
-        HTML = navegador.find_element(By.XPATH,
-            '/html').get_attribute("baseURI")
-        
-        DATA = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[5]').get_attribute("textContent")
-        d1 = DATA[0:10]
-        DATA = d1
-        
-        horainicial = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[5]').get_attribute("textContent")
-        h1 = horainicial[11:19]
-        horainicial = h1
+        AUDIO = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[9]').get_attribute("textContent")
+        AUDIO = "C:\\Users\\PC\\Desktop\\CLIENTE\\Gravacoes\\Gravacoes\\" + AUDIO[0:8] + '.wav'
+
+        MENSAGEM = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[2]/td/table/tbody/tr[2]/td').get_attribute("textContent")
+
+        HTML = navegador.find_element(By.XPATH, '/html').get_attribute("baseURI")
+
+        DATA = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[5]').get_attribute("textContent")[0:10]
+
+        horainicial = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[5]').get_attribute("textContent")[11:19]
         tempo1_delta = datetime.strptime(horainicial, '%H:%M:%S') - datetime.strptime('00:00:00', '%H:%M:%S')
         HORAINICIAL = tempo1_delta
-        
-        duração = navegador.find_element(By.XPATH,
-            '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[6]').get_attribute("textContent")
-        tempo2_delta = datetime.strptime(duração, '%H:%M:%S') - datetime.strptime('00:00:00', '%H:%M:%S')
-        DURAÇÃO = tempo2_delta
-        
+
+        duracao = navegador.find_element(By.XPATH, '/html/body/center/table[2]/tbody/tr[1]/td/table/tbody/tr[2]/td[6]').get_attribute("textContent")
+        tempo2_delta = datetime.strptime(duracao, '%H:%M:%S') - datetime.strptime('00:00:00', '%H:%M:%S')
+        DURAÇAO = tempo2_delta
+
         tempo_soma = tempo1_delta + tempo2_delta
-        tempo_soma_str = str(tempo_soma)
-        tempo_soma_str = tempo_soma_str[-8:]
-        HORAFINAL = tempo_soma_str
-        
-        
-        
+        HORAFINAL = str(tempo_soma)[-8:]
+
         navegador.quit()
-        
-        
-        
-        
+
+        # Inserção dos dados no banco de dados SQLite
         conexao = sqlite3.connect('Banco_CHAMADAS.db')
         c = conexao.cursor()
-        c.execute(" INSERT INTO CHAMADAS VALUES (:ALVO, :TERMINAL, :TELINTERLOCUTORES, :INTERLOCUTORES, :AUDIO, :MENSAGEM, :HTML, :DATA, :HORAINICIAL, :HORAFINAL, :DURAÇÃO)",
-                     {
-                         'ALVO':ALVO,
-                 
-                         'TERMINAL':TERMINAL,
-                 
-                         'TELINTERLOCUTORES':TELINTERLOCUTOR,
-                         
-                         'INTERLOCUTORES':INTERLOCUTORES,
-                         
-                         'AUDIO':AUDIO,
-                         
-                         'MENSAGEM':MENSAGEM,
-                         
-                         'HTML':HTML,
-                         
-                         'DATA':DATA,
-                 
-                         'HORAINICIAL':horainicial,
-                         
-                         'HORAFINAL':tempo_soma_str,
-                        
-                         'DURAÇÃO':duração
-                         
-                     })
-            
+        c.execute('''
+            INSERT INTO CHAMADAS (
+                ALVO, TERMINAL, TELINTERLOCUTOR, INTERLOCUTORES, AUDIO, MENSAGEM, HTML, DATA, HORAINICIAL, HORAFINAL, DURACAO
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (ALVO, TERMINAL, TELINTERLOCUTOR, INTERLOCUTORES, AUDIO, MENSAGEM, HTML, DATA, HORAINICIAL, HORAFINAL, DURAÇAO))
+
         conexao.commit()
         conexao.close()
 
-#EXPORTAÇÃO TABELA, a tabela aparece na mesma pasta onde o programa é executado, atenção aos nomes dados como está abaixo, “CHAMADAS.XLSX”
-
+# Exportação dos registros do banco para um arquivo Excel
 conexao = sqlite3.connect('Banco_CHAMADAS.db')
 c = conexao.cursor()
 c.execute("SELECT *, oid FROM CHAMADAS")
 registros_exportados = c.fetchall()
-registros_exportados = pd.DataFrame(registros_exportados, columns=['ALVO','TERMINAL','TELINTERLOCUTORES','INTERLOCUTORES','AUDIO','MENSAGEM','HTML','DATA','HORAINICIAL','HORAFINAL','DURAÇÃO','Id_banco'])
+registros_exportados = pd.DataFrame(registros_exportados, columns=[
+    'ALVO', 'TERMINAL', 'TELINTERLOCUTOR', 'INTERLOCUTORES', 'AUDIO', 'MENSAGEM', 'HTML', 'DATA', 'HORAINICIAL', 'HORAFINAL', 'DURAÇAO', 'Id_banco'
+])
 registros_exportados.to_excel('CHAMADAS.xlsx')
 conexao.commit()
 conexao.close()
-
-
-
-
-
